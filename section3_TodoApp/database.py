64f59ca17f5ce425ca0e2ef2604ee805d6e52d91
Jake <jakechiu0113@gmail.com>
2024-08-15 09:58:43 +0000

    Update database to Azure SQL DB

--- section3_TodoApp/database.py
+++ section3_TodoApp/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import urllib
+import pyodbc
 
+"""
 
 # sqlite3
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
-
+"""
 
 """
 # Postgres
@@ -19,6 +22,14 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:!Test1234!@localhost:3306/TodoApplicationDatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 """
+
+
+# Azure SQL DB
+params = urllib.parse.quote_plus\
+    (r'Driver={ODBC Driver 18 for SQL Server};Server=tcp:jaketest001.database.windows.net,1433;Database=TestDB;Uid=codingwithroby;Pwd=Test1234!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+SQLALCHEMY_DATABASE_URL = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,echo=True)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
