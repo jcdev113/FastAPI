4ddb86fe17daa0849d9a282378482a84432c2a50
Jake <jakechiu0113@gmail.com>
2024-08-15 05:29:05 +0000

    Update database settings

--- section3_TodoApp/database.py
+++ section3_TodoApp/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-"""
+
 # sqlite3
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
-"""
+
 
 """
 # Postgres
@@ -14,9 +14,11 @@ SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234@localhost/TodoApplicat
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 """
 
+"""
 # mysql
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:!Test1234!@localhost:3306/TodoApplicationDatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+"""
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
